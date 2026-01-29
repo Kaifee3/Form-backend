911d86f9deca24300074bbb8716ebf70f41abddb
Kaifee3 <kaifiazam138@gmail.com>
2026-01-29 13:49:42 +0530

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors"
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -15,9 +16,12 @@ const dbpass=encodeURIComponent(process.env.DBPASS)
 //     console.log("Server started");
 //   });
 // });
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.gwloipc.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@monestryes.8dapf6y.mongodb.net/mon?appName=Monestryes`).then(() => {
   app.listen(8080, () => {
     console.log("Server started");
   });
 });
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+// mongodb+srv://kaifiazam130_db_user:N1fMUQtaed3LFFFL@monestryes.8dapf6y.mongodb.net/?appName=Monestryes
